30c285e405ae956954ab19a0038d25158d519c7d
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-01-11 15:14:53 -0300

    Criando Entidades relacionadas

--- Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
+++ Infra/obj/Debug/net8.0/Infra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+419b0e3a010e3233d0d30a22f6ac306e81c3d963")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
