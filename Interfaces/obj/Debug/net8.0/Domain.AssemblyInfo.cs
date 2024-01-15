2193f6283bca7117d3316e4de3d10a99d3e17014
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-01-15 19:49:08 -0300

    Mais arquivos com problema

--- Interfaces/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Interfaces/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93ce20121858a0c568194030a953a437d793e4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
