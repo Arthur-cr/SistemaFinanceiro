4821756ef09a5cd619676ac48ca3ab83041e5708
Arthur Caetano <arthurcaetano.acrr@gmail.com>
2024-01-16 12:02:44 -0300

    Terminado a class TokenController que farifica o cadastro

---
+++ WebApi/Controllers/TokenController.cs
@@ -0,0 +1,55 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Token;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public TokenController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // Ferifica se o usuario esta cadastrado na tabela do identity
+        [AllowAnonymous]
+        [Produces("application/json")]
+        [HttpPost("/api/CreatToken")]
+        public async Task<IActionResult> CreatToken([FromBody] InputModel Input)
+        {
+            if (string.IsNullOrWhiteSpace(Input.Email) || string.IsNullOrWhiteSpace(Input.Password))
+            {
+                return Unauthorized();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, false, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                var token = new TokenJWTBuilder()
+                    .AddSecurityKey(JwtSecurityKey.Create("Secret_Key-12345678"))
+                    .AddSubject("CAnal Dev Net Core")
+                    .AddIssuer("Teste.Scuriry.Bearer")
+                    .AddAudience("Teste.Scuriry.Bearer")
+                    .AddClaim("UsuarioAPINumero", "1")
+                    .AddExpiry(5)
+                    .Builder();
+
+                return Ok(token.value);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
